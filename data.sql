c18343eb72a1d9728a774543a1d0f6a689379e8e
Senz One <sem63sam@yandex.ru>
2024-03-23 16:41:55 +0400

    sql task completed

--- data.sql
+++ data.sql
@@ -66,24 +66,23 @@ create table donkeys
 );
 
 insert into cats (type, name, birthDate, commands)
-values ('Smudge', 'Cat', '2020-02-20', 'Sit, Pounce, Scratch'),
+values ('Smudge', 'Cat', '2023-01-10', 'Sit, Pounce, Scratch'),
        ('Whiskers', 'Cat', '2019-05-15', 'Sit, Pounce'),
        ('Oliver', 'Cat', '2020-06-30', 'Meow, Scratch, Jump');
 
 insert into dogs (type, name, birthDate, commands)
 values ('Fido', 'Dog', '2020-01-01', 'Sit stay fetch'),
-       ('Buddy', 'Dog', '2018-12-10', 'Sit, Paw, Bark'),
+       ('Buddy', 'Dog', '2023-01-15', 'Sit, Paw, Bark'),
        ('Bella', 'Dog', '2019-11-11', 'Sit, Stay, Roll');
 
 insert into hamsters (name, type, birthDate, commands)
-values
-    ('Hammy', 'Hamster', '2021-03-10', 'Roll, Hide'),
-    ('Peanut', 'Hamster', '2021-08-01', 'Roll, Spin');
+values ('Hammy', 'Hamster', '2021-03-10', 'Roll, Hide'),
+       ('Peanut', 'Hamster', '2021-08-01', 'Roll, Spin');
 
 insert into horses (type, name, birthDate, commands)
 values ('Thunder', 'Horse', '2015-07-21', 'Trot, Canter, Gallop'),
-       ('Storm', 'Horse', '2014-05-05', 'Trot, Canter'),
-       ('Blaze', 'Horse', '2016-02-29', 'Trot, Jump, Gallop');
+       ('Storm', 'Horse', '2024-01-05', 'Trot, Canter'),
+       ('Blaze', 'Horse', '2023-12-20', 'Trot, Jump, Gallop');
 
 insert into camels (type, name, birthDate, commands)
 values ('Dune', 'Camel', '2018-12-12', 'Walk, Sit'),
@@ -92,6 +91,47 @@ values ('Dune', 'Camel', '2018-12-12', 'Walk, Sit'),
 
 insert into donkeys (type, name, birthDate, commands)
 values ('Eeyore', 'Donkey', '2017-09-18', 'Walk, Carry Load, Bray'),
-       ('Burro', 'Donkey', '2019-01-23', 'Walk, Bray, Kick');
+       ('Burro', 'Donkey', '2023-12-30', 'Walk, Bray, Kick');
+
+delete
+from camels;
+
+insert into horses (type, name, birthDate, commands)
+select d.type, d.name, d.birthDate, d.commands
+from donkeys d;
+
+drop table if exists pack_animals;
+alter table horses rename to pack_animals;
 
 
+drop table if exists animals_age;
+create table animals_age
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(50) NOT NULL,
+    birthDate  DATE        NOT NULL,
+    age_months INT
+);
+
+insert into animals_age (name, birthDate, age_months)
+select name, birthDate, TIMESTAMPDIFF(MONTH, birthDate, CURDATE()) AS age_months
+from pack_animals;
+
+drop table if exists animals_1_to_3_years;
+create table animals_1_to_3_years AS
+select *
+from animals_age
+where age_months BETWEEN 1 AND 6;
+
+
+drop table if exists all_animals;
+create table all_animals AS
+select 'cats' AS original_table, id, type, name, birthDate, commands from cats
+union ALL
+select 'dogs' AS original_table, id, type, name, birthDate, commands from dogs
+union ALL
+select 'hamsters' AS original_table, id, type, name, birthDate, commands from hamsters
+union all
+select 'pack_animals' as original_table, id, type, name, birthDate, commands from pack_animals;
+
+select * FROM all_animals;
